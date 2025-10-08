0f96bd0ed370cffa33dfff2e2795e65dad860b81
amytis25 <126446123+amytis25@users.noreply.github.com>
2025-10-08 16:23:28 -0700

    minor change

--- as1/app/reaction_timer.c
+++ as1/app/reaction_timer.c
@@ -9,10 +9,10 @@ int main(){
     /*1. Print a “get ready” message, and do the following four (4) times to flash the BYAI’s LEDs:
         a) Turn on the green LED for 250ms, then turn off.
         b) Turn on the red LED for 250ms, then turn off.*/
-    fprintf(stdout, "Get Ready!\n");
+    printf("Get Ready!\n");
     for (int i = 0; i < 4; i++){
         GreenLed_flash(0, 250000000, 1); // Flash green LED every 0.25 seconds
         RedLed_flash(0, 250000000, 1);   // Flash red LED every 0.25 seconds
     }
-    
+
 }
