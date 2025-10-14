5fbb1fbb0f8684e88f566c87ea35b0019fb7af30
Amytis Saghafi <amytis25@gmail.com>
2025-10-13 18:56:41 -0700

    attempt 3?

--- as1/app/reaction_timer.c
+++ as1/app/reaction_timer.c
@@ -6,10 +6,18 @@
 #include "hal/SPI.h"
 #include <time.h>
 
+typedef struct reaction_returns
+{
+    long long reaction_time;
+    bool status;
+} reaction_returns;
+
 void startup() {  
     /*1. Print a “get ready” message, and do the following four (4) times to flash the BYAI’s LEDs:
     a) Turn on the green LED for 250ms, then turn off.
     b) Turn on the red LED for 250ms, then turn off.*/
+    printf("When the LEDs light up, press the joystick in that direction! (green -> up, red -> down)\n");
+    printf("(Press left or right to exit)\n");
     printf("Get Ready...\n");
     for (int i = 0; i < 4; i++){
         GreenLed_flash(250, 1); // Flash green LED for 250ms, 1 time
@@ -20,59 +28,53 @@ void startup() {
 }
 void promptUser(int direction) {
     if (direction == 0) {
-        printf("Move the joystick UP!\n");
+        printf("Press UP now!\n");
         GreenLed_turnOn();
     } else {
-        printf("Move the joystick DOWN!\n");
+        printf("Press DOWN now!\n");
         RedLed_turnOn();
     }
 }
-long long getReactionTime(int direction){
+reaction_returns getReactionTime(int direction){
     joystick_values jv;
     long long reaction_time_ms;
     long long startTime = getTimeInMs();
+    reaction_returns result = {5000, true};
     // wait for user to move joystick or timeout after 5 seconds
     while(getTimeInMs() - startTime < 5000){ // 5 seconds to respond
         
         jv = Read_ADC_Values();
         if (jv.x > 50 || jv.x < -50) {
-            printf("You moved the joystick on the x-axis!\n");
-            return 5000;
-            break;
+            printf("User selected to quit.\n");
+            result = {5000, false};
+            return result;
         }
-        else if (direction == 0 && jv.y > 50) { // Moved up when prompted up
+        else if ((direction == 0 && jv.y > 50)||(direction == 1 && jv.y < -50)) { // correct direction
             reaction_time_ms = getTimeInMs() - startTime;
-            printf("You moved the joystick UP! Reaction time: %lld ms\n", reaction_time_ms);
+            printf("Correct!\n");
             GreenLed_flash(200, 5); // flash the green LED on and off five times in one second
-            return reaction_time_ms;
+            result = {reaction_time_ms, true};
+            return result;
             break;
         } 
-        else if (direction == 1 && jv.y < -50) { // Moved down when prompted down 
-            reaction_time_ms = getTimeInMs() - startTime;
-            printf("You moved the joystick DOWN! Reaction time: %lld ms\n", reaction_time_ms);
-            GreenLed_flash(200, 5); // flash the green LED on and off five times in one second
-            return reaction_time_ms;
+        else if ((direction == 0 && jv.y < -50)||(direction == 1 && jv.y > 50) ){ // wrong direction
+            printf("Incorrect direction!.\n");
+            reaction_returns result = {5000, true};
+            return result;
             break;
-        } else if (direction == 0 && jv.y < -50){ // Moved down when prompted up
-            printf("Wrong direction! You moved the joystick DOWN when prompted UP.\n");
-            return 5000;
-            break;
-        } else if (direction == 1 && jv.y > 50){ // Moved up when prompted down
-            printf("Wrong direction! You moved the joystick UP when prompted DOWN.\n");
-            return 5000;
-            break;
-
-        }
+        } 
     }
-    printf("Timeout! You did not move the joystick in time.\n");
-    return 5000; // Return a high value indicating timeout
-
-} 
-long long game (long long highestRecord){
+    printf("No input within 5 seconds; quitting!\n");
+    result = {5000, false}; // Return a high value indicating timeout
+    return result;
+}
+reaction_returns game (long long highestRecord){
     joystick_values jv;
     long long reaction_time_ms;
     int wait_min_ms = 500;
     int wait_max_ms = 3000;
+    reaction_returns result = {5000, true};
+    reaction_returns highScore = {5000, true};
     
     /* seed RNG so rand() produces different sequences across runs */
     srand(getTimeInMs() % 1000); // seed with current time in ms mod 1000
@@ -97,13 +99,18 @@ long long game (long long highestRecord){
     promptUser(randomDir);
 
     // initialize game and get reaction time
-    reaction_time_ms = getReactionTime(randomDir);
+    result = getReactionTime(randomDir);
 
     // check if reaction time is a new record
-    if (reaction_time_ms < highestRecord) {
-                highestRecord = reaction_time_ms;
+    if (result.reaction_time_ms < highestRecord) {
+                highScore.reaction_time_ms = result.reaction_time_ms;
                 printf("New record! Fastest reaction time: %lld ms\n", highestRecord);
             }
+            else {
+                printf("Your reaction time was %lld ms. Fastest reaction time remains: %lld ms\n", reaction_time_ms, highestRecord);
+            }
+    // update game status
+    highScore.status = result.status;
     // cleanup 
     if (randomDir == 0) { 
             GreenLed_turnOff(); 
@@ -111,17 +118,17 @@ long long game (long long highestRecord){
         } else if (randomDir == 1) {
             RedLed_turnOff();
         }
-        return highestRecord;
+        return highScore;
 }
 
 int main(){
     long long HighestRecord = 5000;
-    char status = 'p';
+    bool status = true;
+    reaction_returns current = {5000, true};
 
-    while (status == 'p') {
-        HighestRecord = game(HighestRecord);
-        printf("Press ENTER to play again or CTRL-C to exit.\n");
-        status = getchar();
+
+    while (current.status == true) {
+        current = game(HighestRecord);
     }
 
     return 0;
